3f60df391e4917da60bd0660af377f81df9acbb5
Brandon <bburkhardt5@gmail.com>
2016-03-19 12:58:04 -0600

    IR Sensors Added

---
+++ IRSensors.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   IRSensors.h
+ * Author: Brandon
+ *
+ * Created on March 18, 2016, 6:58 PM
+ */
+
+#ifndef IRSENSORS_H
+#define	IRSENSORS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include "Header.h"
+    
+    void config_IR();
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* IRSENSORS_H */
+
